362da6488164f7466932c5093032ccd7f12a1b68
Florent Heyraud <florentheyraud.pro@gmail.com>
2020-02-24 00:11:21 +0100

    feat(map.js): adding markers

--- components/main.js
+++ components/main.js
@@ -1,2 +1,2 @@
 new Slider("slider-images", 5000);
-new BikeMap("map-container", "map", 48.8534, 2.3488, 12);
+new BikeMap("map-container", "map", 47.2173, -1.5534, 12, "https://api.jcdecaux.com/vls/v1/stations?contract=Nantes&apiKey=MY_KEY");
--- components/map.js
+++ components/map.js
@@ -1,19 +1,79 @@
 class BikeMap {
 
-    constructor(mapContainer, mapID, lat, lon, zoom) {
+    constructor(mapContainer, mapID, lat, lon, zoom, bikeURL) {
         this.container = document.getElementById(mapContainer);
         this.mapID = document.getElementById(mapID);
         this.mapLat = lat;
         this.mapLon = lon;
         this.mapZoom = zoom;
         this.map = L.map(this.mapID).setView([this.mapLat, this.mapLon], this.mapZoom);
-        this.layer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={myToken}', {
+        this.bikeURL = bikeURL;
+        
+        this.setMapLayer();
+        this.getBikeData();
+
+        this.greenMarker = new L.Icon({
+            iconUrl: 'assets/marker-icon-2x-green.png',
+            shadowUrl: 'assets/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        });
+
+        this.redMarker = new L.Icon({
+            iconUrl: 'assets/marker-icon-2x-red.png',
+            shadowUrl: 'assets/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        });
+
+        this.orangeMarker = new L.Icon({
+            iconUrl: 'assets/marker-icon-2x-orange.png',
+            shadowUrl: 'assets/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        });
+        
+    }
+
+    // 
+    setMapLayer() {
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=MY_TOKEN', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
             id: 'mapbox/streets-v11',
             tileSize: 512,
             zoomOffset: -1,
-            accessToken: 'myToken'
+            accessToken: 'MY_TOKEN'
         }).addTo(this.map);
     }
+
+    getBikeData() {
+        fetch(this.bikeURL)
+            .then(response => response.json())
+            .then(data => this.setMarkers(data))
+            .catch(error => alert("Désolé, une erreur est survenue! " + "( " + error + ")"))
+    }
+
+    setMarkers(data) {
+        this.stations = data;
+        this.stations.map(marker => {
+
+            if(marker.available_bikes <= 10) {
+                L.marker([marker.position.lat, marker.position.lng], {icon: this.orangeMarker}).addTo(this.map)
+            }
+            if(marker.available_bikes >= 10) {
+                L.marker([marker.position.lat, marker.position.lng], {icon: this.greenMarker}).addTo(this.map)
+            }
+            if(marker.available_bikes === 0) {
+                L.marker([marker.position.lat, marker.position.lng], {icon: this.redMarker}).addTo(this.map)
+            }
+        });
+    }
+
 }
